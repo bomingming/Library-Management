cb14bf07518fed7cc31c6a1ba1df00edbc1ec566
choiyoungmi <choiyoumgmi2252@gmail.com>
2022-05-30 17:23:18 +0900

    북대여

---
+++ Bookrent.py
@@ -0,0 +1,8 @@
+#반납
+def ReturnBook():
+    global RentDf
+    RentBookIsbn = '9788937460449'
+    if (RentBookIsbn == RentDf['BOOK_ISBN']).any():
+        Index = RentDf[RentDf['BOOK_ISBN'].str.contains(RentBookIsbn)].index            #삭제하고자 하는 도서 데이터의 인덱스 구하기
+        RentDf = RentDf.drop(Index)                                                     #해당 인덱스의 행 삭제
+        RentDf.to_csv('RentList.csv',index=False,encoding='utf-8')
