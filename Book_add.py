0936d14e0cba3f8d5c9e45ec9041f0597f9c4c41
Kim bomin <byj06036@naver.com>
2022-06-05 11:42:26 +0900

    도서 등록 GUI 커맨드 추가

--- Book_add.py
+++ Book_add.py
@@ -64,11 +64,11 @@ def BookInfowindow():
     def AddBook():  #도서 등록 버튼 누를 시
         global BookDf
         if (BookDf['BOOK_ISBN']==IsbnEnter.get()).any():
-            messagebox.showerror('중복된 도서', '중복된 도서입니다. \n (오류 : ISBN 중복)')
+            messagebox.showerror('중복된 도서', '중복된 도서입니다. \n (오류 : ISBN 중복)') #등록 오류 메시지(중복)
         elif '' in [TitleEnter.get(),IsbnEnter.get(),AuthorEnter.get(), 
             AuthorEnter.get(), PriceEnter.get(), LinkEnter.get(),
             BookInfomationEnter.get(1.0, 50.50)]:
-            messagebox.showerror('등록 오류', '올바른 정보를 입력하세요.')
+            messagebox.showerror('등록 오류', '올바른 정보를 입력하세요.')  #등록 오류 메시지(누락)
 
         else:
             AddDf = pd.DataFrame({'BOOK_TITLE':[TitleEnter.get()],
@@ -77,22 +77,24 @@ def BookInfowindow():
                 'BOOK_PUB':[AuthorEnter.get()],
                 'BOOK_PRICE':[PriceEnter.get()],
                 'BOOK_LINK':[LinkEnter.get()],
-                'BOOK_INFOR':[BookInfomationEnter.get(1.0, 50.50)],
+                'BOOK_INFOR':[BookInfomationEnter.get(1.0, 50.50)],     #임의로 설정한 저장값
                 'BOOK_RENT':[False],
                 'BOOK_PIC':[None]})
-            BookDf = pd.concat([BookDf, AddDf])
+            BookDf = pd.concat([BookDf, AddDf])         #등록 정보를 기존 데이터프레임에 합치기
 
-            BookDf.to_csv('BookList.csv',index=False,encoding='utf-8')
+            BookDf.to_csv('BookList.csv',index=False,encoding='utf-8')  #csv파일에 저장
+
+            messagebox.showinfo('등록 완료', '등록이 완료되었습니다.')  #등록 완료 메시지
 
     # 버튼
-    ImageButton = Button(BIWindow, image = '')                          # 도서 이미지 추가버튼
+    ImageButton = Button(BIWindow, image = '')                       # 도서 이미지 추가버튼
     ImageButton.place(x = 130, y = 80, width = 170, height = 200)
 
-    OkButton = Button(BIWindow, text = '등록', command=AddBook)                            # 확인 버튼
-    OkButton.place(x = 130, y = 290, width = 50)
+    OkButton = Button(BIWindow, text = '등록', command=AddBook)      # 등록 버튼
+    OkButton.place(x = 155, y = 290, width = 50)
 
-    OutButton = Button(BIWindow, text = '취소')                           # 삭제 버튼
-    OutButton.place(x = 250, y = 290, width = 50)
+    OutButton = Button(BIWindow, text = '취소')                       # 취소 버튼
+    OutButton.place(x = 230, y = 290, width = 50)
     
     BIWindow.mainloop()
 
