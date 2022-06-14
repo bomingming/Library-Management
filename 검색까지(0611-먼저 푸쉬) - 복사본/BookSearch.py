a86613547ec854b484f45f0914b309130fea9247
choiyoungmi <choiyoumgmi2252@gmail.com>
2022-06-14 13:47:12 +0900

    1차검색창만듦

---
+++ 검색까지(0611-먼저 푸쉬) - 복사본/BookSearch.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+f=open('TestBookList.csv','r',encoding='utf-8')
+BookDf=pd.read_csv(r'.\TestBookList.csv')# data에 읽은 값 저장
+
+# 도서 검색
+def Search(InStandard,InSearch): 
+    
+    if InStandard=="도서 명":                  # 도서 명 선택 시
+        SearchIndex="BOOK_TITLE"               # 도서 데이터 다루기
+    elif InStandard=="저자":                   # 저자 선택 시
+        SearchIndex="BOOK_AUTHOR"              # 저자 데이터 다루기
+    elif InStandard=="출판사":                 # 출판사 선택 시
+        SearchIndex="BOOK_PUB"                 # 출판사 데이터 다루기
+
+    if BookDf[SearchIndex].str.contains(InSearch).any():
+        return BookDf.loc[BookDf[SearchIndex].str.contains(InSearch)]
+    elif InSearch == '':
+        return BookDf
